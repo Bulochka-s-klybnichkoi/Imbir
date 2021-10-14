fafe380085973449a404439f504cc5253d26532c
Bulochka-s-klubnichkoi <sofia.biletskaya15092010@gmail.com>
2021-10-15 00:25:27 +0300

    тупое дерево 2

--- assets/js/tree.js
+++ assets/js/tree.js
@@ -1,6 +1,7 @@
-var leaf2 = document.getElementsByClassName("leaf2-container");
+var leaf2 = document.getElementsByClassName("leaf2-container")[0];
 leaf2.addEventListener("click", leaf2Click);
 
 function leaf2Click() {
    leaf2.style.transform = "translate(-168px, -283px) rotate(-53deg)";
+   leaf2.style.transition = "transform 900ms ease"
 }
